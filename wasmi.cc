7e5d5da04221b1dafbc404ebd523c80581cf6f6e
Athang Gupte <xdevapps0@gmail.com>
2026-07-13 23:54:10 -0700

    Added wasm primitive types, and typepack helper

--- wasmi.cc
+++ wasmi.cc
@@ -1,30 +1,105 @@
 #include <cstdint>
+#include <intrin.h>
 
 namespace wasmi {
 
-enum WaPrimitive {
-	eI32,
-	eI64,
-	eF32,
-	eF64
+template <typename... _Types>
+struct WaTypePack {
+	using Types = _Types...;
 };
 
-using wa_byte = std::uint8_t:
-using wa_i32 = std::int32_t;
-using wa_i64 = std::int64_t;
-using wa_f32 = std::float_t;
-using wa_f64 = std::double_t;
+namespace TypePack {
+
+/// MaxSize
+
+template <typename... _Ts>
+struct MaxSizeHelper;
+
+template <typename _T0, typename... _Ts>
+struct MaxSizeHelper<_T0, _Ts...> {
+	static constexpr size_t value = sizeof(_T0) > MaxSizeHelper<_Ts...>::value ? sizeof(_T0) : MaxSizeHelper<_Ts...>::value;
+};
+
+template <>
+struct MaxSizeHelper<> {
+	static constexpr size_t value = 0;
+};
+
+template <typename _TypePack>
+struct MaxSize;
 
 template <typename... _Types>
-struct WaTypePack;
+struct MaxSize<WaTypePack<_Types...>> {
+	static constexpr size_t value = MaxSizeHelper<_Types...>::value;
+};
 
-template <typename _Tag, typename... _Types>
-class WaTaggedUnion : public WaTaggedUnion<_Tag, WaTypePack<_Types...>> {
+static_assert(MaxSize<WaTypePack<int32_t, int64_t, float, double>>::value == sizeof(double), "MaxSize calculation is incorrect");
+
+/// MaxAlign
+
+template <typename... _Types>
+struct MaxAlignHelper;
+
+template <typename _T0, typename... _Ts>
+struct MaxAlignHelper<_T0, _Ts...> {
+	static constexpr size_t value = alignof(_T0) > MaxAlignHelper<_Ts...>::template value ? alignof(_T0) : MaxAlignHelper<_Ts...>::template value;
+};
+
+template <>
+struct MaxAlignHelper<> {
+	static constexpr size_t value = 0;
+};
+
+template <typename _TypePack>
+struct MaxAlign;
+
+template <typename... _Types>
+struct MaxAlign<WaTypePack<_Types...>> {
+	static constexpr size_t value = MaxAlignHelper<_Types...>::template value;
+};
+
+/// TypeIndex
+
+template <typename _Type, typename... _Types>
+struct TypeIndexHelper;
+
+template <typename _Type, typename _T0, typename... _Ts>
+struct TypeIndexHelper<_Type, _T0, _Ts...> {
+	static constexpr size_t value = std::is_same_v<_Type, _T0> ? 0 : 1 + TypeIndexHelper<_Type, _Ts...>::template value;
+};
+
+template <typename _Type>
+struct TypeIndexHelper<_Type> {
+	static constexpr size_t value = 0;
+};
+
+template <typename _Type, typename _TypePack>
+struct TypeIndex;
+
+}
+
+template <typename _TypePack>
+class WaUnionStorage {
+public:
+	WaUnionStorage() = default;
+
+	template <typename _Type>
+	WaUnionStorage()
+private:
+	alignas(TypePack::MaxAlign<_TypePack>::value) char m_storage[TypePack::MaxSize<_TypePack>::value];
+};
+
+template <typename... _Types>
+class WaUnion : public WaUnion<WaTypePack<_Types...>> {
+public:
+};
+
+template <typename... _Types>
+class WaUnion<WaTypePack<_Types...>> {
+	using TypePack = WaTypePack<_Types...>;
+public:
+protected:
 	
-};
-
-template <typename... _Types>
-class WaUnion : public WaUnion<WaTypePack<_Types...> {
 };
 
 template <typename _Type>
@@ -36,11 +111,85 @@ class WaSharedRef;
 template <typename _Type>
 class WaWeakRef;
 
-using WaPrimTypePack = WaTypePack<wa_i32, wa_i64, wa_f32, wa_f64>;
+
+enum WaNumberType {
+	ntI32, 		// = 0x7F
+	ntI64, 		// = 0x7E
+	ntF32, 		// = 0x7D
+	ntF64, 		// = 0x7C
+};
+
+enum WaVectorType {
+	vtV128,		// = 0x7B
+};
+
+enum WaHeapType {
+	htArray, 	// = 0x6A,
+	htStruct, 	// = 0x6B,
+	htI31, 		// = 0x6C,
+	htEq, 		// = 0x6D,
+	htAny, 		// = 0x6E,
+	htExtern, 	// = 0x6F,
+	htFunc,		// = 0x70,
+	htNone, 	// = 0x71,
+	htNoextern, // = 0x72,
+	htNofunc, 	// = 0x73,
+	htNoexn, 	// = 0x74,
+};
+
+enum WaRefType {
+	rtArray, 	// = 0x6A,
+	rtStruct, 	// = 0x6B,
+	rtI31, 		// = 0x6C,
+	rtEq, 		// = 0x6D,
+	rtAny, 		// = 0x6E,
+	rtExtern, 	// = 0x6F,
+	rtFunc,		// = 0x70,
+	rtNone, 	// = 0x71,
+	rtNoextern, // = 0x72,
+	rtNofunc, 	// = 0x73,
+	rtNoexn, 	// = 0x74,
+};
+
+template <uint32_t _Tag, typename _UnderlyingType>
+struct WaPrimitiveType {
+	using UnderlyingType = _UnderlyingType;
+	static constexpr uint32_t Tag = _Tag;
+	_UnderlyingType value;
+};
+
+#define WA_PRIMITIVE_TYPES(MACRO) \
+	MACRO(WaI32, ntI32, int32_t) \
+	MACRO(WaI64, ntI64, int64_t) \
+	MACRO(WaF32, ntF32, float) \
+	MACRO(WaF64, ntF64, double) \
+	MACRO(WaV128, vtV128, __m128) \
+	MACRO(WaAnyRef, rtAny, int32_t) \
+	MACRO(WaEqRef, rtEq, int32_t) \
+	MACRO(WaI32Ref, rtI31, int32_t) \
+	MACRO(WaStructRef, rtStruct, int32_t) \
+	MACRO(WaArrayRef, rtArray, int32_t) \
+	MACRO(WaNullRef, rtNone, int32_t) \
+	MACRO(WaFuncRef, rtFunc, int32_t) \
+	MACRO(WaNullFuncRef, rtNofunc, int32_t) \
+	MACRO(WaExnRef, rtExn, int32_t) \
+	MACRO(WaNullExnRef, rtNoexn, int32_t) \
+	MACRO(WaExternRef, rtExtern, int32_t) \
+	MACRO(WaNullExternRef, rtNoextern, int32_t)
+
+#define WA_DECLARE_PRIMITIVE_TYPE_ALIAS(_Name, _Tag, _UnderlyingType) \
+	using _Name = WaPrimitiveType<_Tag, _UnderlyingType>;
+
+WA_PRIMITIVE_TYPES(WA_DECLARE_PRIMITIVE_TYPE_ALIAS)
+
+#define WA_PRIMITIVE_TYPE_ALIAS_LIST(_Name, _Tag, _UnderlyingType) \
+	_Name,
+
+using WaValueType = WaUnion<WA_PRIMITIVE_TYPES(WA_PRIMITIVE_TYPE_ALIAS_LIST)>;
 
 class WaParam {
 private:
-	WaUnion<WaPrimitive, WaPrimTypePack> m_prim;
+	WaValueType m_value;
 };
 
 class WaType {
